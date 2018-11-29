562eca4805899c890e98f801e084d4a3aacb5aa2
Ankur Gupta <rukna1000@gmail.com>
2018-11-28 21:50:51 -0500

    Add response object

--- src/main/java/com/lesath/apps/controller/CreateScheduleResponse.java
+++ src/main/java/com/lesath/apps/controller/CreateScheduleResponse.java
@@ -1,5 +1,16 @@
 package com.lesath.apps.controller;
 
 public class CreateScheduleResponse {
-
+	int httpCode;
+	
+	public CreateScheduleResponse(int code) {
+		httpCode = code;
+	}
+	
+	public int getHTTPCode() {
+		return this.httpCode;
+	}
+	public void setHttpCode(int code) {
+		this.httpCode = code;
+	}
 }
