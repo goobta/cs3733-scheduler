f488f2c9c1600235c359538218f5808199d0e0ce
Ankur Gupta <rukna1000@gmail.com>
2018-11-28 19:36:58 -0500

    Add Model for ScheduleConfig JSON model

--- src/main/java/com/lesath/apps/controller/CreateScheduleHandler.java
+++ src/main/java/com/lesath/apps/controller/CreateScheduleHandler.java
@@ -1,20 +1,14 @@
 package com.lesath.apps.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.google.gson.Gson;
-
-import edu.wpi.cs.heineman.demo.CreateConstantResponse;
 
 public class CreateScheduleHandler implements RequestStreamHandler {
 
@@ -26,6 +20,7 @@ public class CreateScheduleHandler implements RequestStreamHandler {
     	logger = context.getLogger();
     	
     	if (logger != null) logger.log("In CreateSchedule handleRequest");
+    	
+ 
     }
-
 }
---
+++ src/main/java/com/lesath/apps/controller/CreateScheduleRequest.java
@@ -0,0 +1,5 @@
+package com.lesath.apps.controller;
+
+public class CreateScheduleRequest {
+
+}
