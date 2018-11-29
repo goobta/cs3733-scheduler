bbdf8c2f3e10a53fbf7909c2eccbd6bc73b6fab0
mahajanrevant <revantmahajan1999@gmail.com>
2018-11-28 20:07:10 -0500

    ScheduleDAO works for adding schedule to the table Schedule

--- src/main/java/com/lesath/apps/db/Main.java
+++ src/main/java/com/lesath/apps/db/Main.java
@@ -1,36 +1,45 @@
 package com.lesath.apps.db;
 
 import java.sql.Connection;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
+import com.lesath.apps.model.Schedule;
 import com.lesath.apps.model.TestTable;
 
-
+/**
+ * 
+ * @author revant
+ *	This class is to test out dao objects. Please don't write usefull code here.
+ */
 public class Main {
 
 	public static void main(String args[]) {
 		
-		Connection conn ;
+		/*Connection conn ;
     	try  {
     		conn = DatabaseUtil.connect();
     		System.out.println("done");
     	} catch (Exception e) {
     		conn = null;
     		System.out.println("none");
-    	}
+    	}*/
     	
-    	TestTable t = new TestTable(5,7);
+    	Schedule sche = new Schedule(null, "work", 15, LocalDate.now(), LocalDate.now(), LocalTime.now(), LocalTime.now(), LocalDateTime.now(), null );
     	
-    	/*try {
+    	try {
     		ScheduleDAO s = new ScheduleDAO();
-    		s.addSchedule(t);
+    		s.addSchedule(sche);
     		System.out.println("added");
     	}
     	catch(Exception e){
+    		e.printStackTrace();
     		System.out.println("fails to put");
-    	}*/
+    	}
     	
-    	try {
+    	/*try {
     		ScheduleDAO s = new ScheduleDAO();
     		ArrayList<TestTable> lis = new ArrayList<TestTable>();
     		lis = s.getSchedule();
@@ -42,7 +51,7 @@ public class Main {
     	}
     	catch(Exception e){
     		System.out.println("fails to get");
-    	}
+    	}*/
     	
 	}
 }
