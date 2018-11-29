9442012ce669c2639a83b8460b1d342ef1a23ad3
Ankur Gupta <rukna1000@gmail.com>
2018-11-28 21:13:39 -0500

    Add convertor

--- src/main/java/com/lesath/apps/util/LocalDateTimeJsonConvertor.java
+++ src/main/java/com/lesath/apps/util/LocalDateTimeJsonConvertor.java
@@ -1,5 +1,28 @@
 package com.lesath.apps.util;
 
-public class LocalDateTimeJsonConvertor {
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+ 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
+public class LocalDateTimeJsonConvertor implements JsonSerializer<LocalDateTime>,JsonDeserializer<LocalDateTime> {
+    
+	private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+	
+	public JsonElement serialize(LocalDateTime localDateTime, Type type, JsonSerializationContext jsonSerializationContext) {
+        return new JsonPrimitive(localDateTime.format(dateTimeFormatter));
+    }
+ 
+	public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+			throws JsonParseException {
+		return LocalDateTime.parse(json.getAsString(),dateTimeFormatter);
+		
+	}
 }
