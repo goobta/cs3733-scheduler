a04ae0c9334056eb867a1738db260ab1ab53fd7c
Andy <avanosten@wpi.edu>
2018-12-04 17:00:19 -0500

    Created and implemented TimesNotAvailableDAO

--- src/main/java/com/lesath/apps/db/Main.java
+++ src/main/java/com/lesath/apps/db/Main.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import com.lesath.apps.model.Meeting;
 import com.lesath.apps.model.Schedule;
 import com.lesath.apps.model.TestTable;
+import com.lesath.apps.model.TimeNotAvailable;
 
 /**
  * 
@@ -39,6 +40,7 @@ public class Main {
     		//schedules = s.getSchedule();
     		
     		MeetingDAO mdao = new MeetingDAO();
+    		//mdao.addMeeting(new Meeting("a","b",LocalDateTime.now(), LocalDateTime.now(), null, "JimJim"));
     		//Meeting m = mdao.getMeeting("91194d6f-dafb-47f2-809c-38029d0a5df3");
     		//System.out.println(m.getParticipant_name() + "'s meeting");
     		System.out.println(mdao.getAllMeetings().get(0).getParticipant_name());
@@ -46,6 +48,10 @@ public class Main {
     			//System.out.println(schee);
     		//}
     		
+    		TimesNotAvailableDAO tnadao = new TimesNotAvailableDAO();
+    		//tnadao.addTimeNotAvailable(new TimeNotAvailable("JimTime",null,LocalDateTime.now(),LocalDateTime.now(),null));
+    		//System.out.println(tnadao.getTimeNotAvailable("35333e0d-23b3-4f04-a13c-8058caa8d998").getSchedule_id());
+    		System.out.println(tnadao.getAllTimesNotAvailable().get(0).getSchedule_id());
     	}
     	catch(Exception e){
     		//e.printStackTrace();
