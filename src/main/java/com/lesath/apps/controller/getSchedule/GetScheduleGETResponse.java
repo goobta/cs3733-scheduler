68cada16a7e0b92d539b29df9de754a8cbfcd3e7
Ankur Gupta <rukna1000@gmail.com>
2018-12-06 12:28:39 -0500

    Add extended schedule

---
+++ src/main/java/com/lesath/apps/controller/getSchedule/GetScheduleGETResponse.java
@@ -0,0 +1,71 @@
+package com.lesath.apps.controller.getSchedule;
+
+import com.google.gson.annotations.SerializedName;
+import com.lesath.apps.db.MeetingDAO;
+import com.lesath.apps.db.TimesNotAvailableDAO;
+import com.lesath.apps.model.Meeting;
+import com.lesath.apps.model.Schedule;
+import com.lesath.apps.model.TimeNotAvailable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class GetScheduleGETResponse {
+    @SerializedName("id")
+    String uuid;
+
+    String name;
+    String organizerId = "01e113d8-3cc3-4fc1-90a2-81f65cfcda16";
+    LocalDateTime startDateTime;
+    LocalDateTime endDateTime;
+
+    @SerializedName("meetingDuration")
+    int duration;
+
+    LocalDateTime created_at;
+    LocalDateTime deleted_at;
+
+    List<Meeting> meetings;
+    List<TimeNotAvailable> timesNotAvailables;
+
+    public GetScheduleGETResponse(Schedule s) {
+        this.uuid = s.getUuid();
+        this.name = s.getName();
+        this.duration = s.getDuration();
+        this.created_at = s.getCreated_at();
+        this.deleted_at = s.getDeleted_at();
+
+        this.startDateTime = s.getStartDateTime();
+        this.endDateTime = s.getEndDateTime();
+    }
+
+    boolean loadMeetingsAndTimesNotAvailable () {
+        return this.getMeetings() && this.getTimesNotAvailable();
+    }
+
+    private boolean getMeetings() {
+        MeetingDAO mDAO = new MeetingDAO();
+
+        try {
+            this.meetings = mDAO.getAllMeetingsForSchedule(this.uuid);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean getTimesNotAvailable() {
+        TimesNotAvailableDAO tnaDAO = new TimesNotAvailableDAO();
+
+        try {
+            this.timesNotAvailables = tnaDAO.getAllTimesNotAvailableForScheduleId(this.uuid);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+}
