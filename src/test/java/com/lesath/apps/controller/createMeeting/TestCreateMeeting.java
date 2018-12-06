a8ebfbe253d2fccc90a7cc1e3801e0a07a4b2d25
Ankur Gupta <rukna1000@gmail.com>
2018-12-05 22:19:02 -0500

    Create test for delete meeting

--- src/test/java/com/lesath/apps/controller/createMeeting/TestCreateMeeting.java
+++ src/test/java/com/lesath/apps/controller/createMeeting/TestCreateMeeting.java
@@ -1,23 +1,22 @@
 package com.lesath.apps.controller.createMeeting;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.lesath.apps.controller.LambdaHandler;
+import com.lesath.apps.controller.LambdaResponse;
+import com.lesath.apps.controller.TestAPIGatewayRequest;
+import com.lesath.apps.controller.model.MeetingInput;
+import com.lesath.apps.db.MeetingDAO;
+import com.lesath.apps.model.Meeting;
+import com.lesath.apps.util.HTTPMethod;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.time.LocalDateTime;
-
-import org.json.simple.parser.ParseException;
-import org.junit.Assert;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-import com.amazonaws.services.lambda.runtime.Context;
-import com.lesath.apps.controller.LambdaHandler;
-import com.lesath.apps.controller.LambdaResponse;
-import com.lesath.apps.controller.TestAPIGatewayRequest;
-import com.lesath.apps.controller.createSchedule.CreateSchedulePUTResponse;
-import com.lesath.apps.controller.model.MeetingInput;
-import com.lesath.apps.util.HTTPMethod;
 
 
 public class TestCreateMeeting {
@@ -43,5 +42,29 @@ public class TestCreateMeeting {
      
         Assert.assertNotEquals(resp.meetingUuid, "");
         
+	}
+
+	@Test
+	public void deleteMeeting() throws Exception {
+		TestAPIGatewayRequest request = new TestAPIGatewayRequest();
+		Meeting meeting = new Meeting("82e712aa-d528-4f19-a44c-0835f54b91ee", null, LocalDateTime.now(),
+				LocalDateTime.now(), null, "TestBoi");
+
+		MeetingDAO dao = new MeetingDAO();
+		String meetingId = dao.addMeeting(meeting);
+
+		request.addQueryParameter("scheduleId", "82e712aa-d528-4f19-a44c-0835f54b91ee");
+		request.addQueryParameter("meetingId", meetingId);
+
+		InputStream input = request.generateRequest(HTTPMethod.DELETE);
+		OutputStream output = new ByteArrayOutputStream();
+		Context context = request.generateContext("CreateScheduleDELETE");
+
+		CreateMeetingHandler handler = new CreateMeetingHandler();
+		handler.handleRequest(input, output, context);
+
+		LambdaResponse respose = LambdaHandler.gson.fromJson(output.toString(), LambdaResponse.class);
+
+		Assert.assertEquals(respose.statusCode, 204);
 	}
 }
