2c0916d2899774759f321d6dbaf2050d35bb9f20
mahajanrevant <revantmahajan1999@gmail.com>
2018-12-11 00:13:44 -0500

    cancelMeetingOrg finished

---
+++ src/main/java/com/lesath/apps/controller/cancelOrgMeeting/CancelOrgMeetingHandler.java
@@ -0,0 +1,54 @@
+package com.lesath.apps.controller.cancelOrgMeeting;
+
+import java.util.ArrayList;
+
+import com.lesath.apps.controller.APIGatewayRequest;
+import com.lesath.apps.controller.LambdaHandler;
+import com.lesath.apps.controller.createMeeting.CreateMeetingRequest;
+import com.lesath.apps.controller.createMeeting.CreateMeetingResponse;
+import com.lesath.apps.controller.model.MeetingInput;
+import com.lesath.apps.db.MeetingDAO;
+import com.lesath.apps.util.HTTPMethod;
+
+public class CancelOrgMeetingHandler extends LambdaHandler {
+	@Override
+    protected boolean init() {
+        this.controllerName = "CancelOrgMeeting";
+
+        this.handledMethods = new ArrayList<>();
+        handledMethods.add(HTTPMethod.DELETE);
+
+        return true;
+    }
+    
+    @Override
+    protected boolean handlePUT(APIGatewayRequest request) {
+    	return false;
+        
+    }
+
+    @Override
+    protected boolean handlePOST(APIGatewayRequest request) {
+        return false;
+    }
+
+    @Override
+    protected boolean handleGET(APIGatewayRequest request) {
+        return false;
+    }
+
+    @Override
+    protected boolean handleDELETE(APIGatewayRequest request) {
+        String scheduleId = request.queryStringParameters.get("scheduleId");
+        String meetingId = request.queryStringParameters.get("meetingId");
+
+        MeetingDAO dao = new MeetingDAO();
+        if (dao.deleteMeeting(meetingId)) {
+            this.response.setStatusCode(204);
+            return true;
+        } else {
+            this.response.setStatusCode(404);
+            return false;
+        }
+    }
+}
---
+++ src/test/java/com/lesath/apps/controller/cancelOrgMeeting/TestCancelOrgMeeting.java
@@ -0,0 +1,45 @@
+package com.lesath.apps.controller.cancelOrgMeeting;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.lesath.apps.controller.LambdaHandler;
+import com.lesath.apps.controller.LambdaResponse;
+import com.lesath.apps.controller.TestAPIGatewayRequest;
+import com.lesath.apps.controller.createMeeting.CreateMeetingHandler;
+import com.lesath.apps.db.MeetingDAO;
+import com.lesath.apps.model.Meeting;
+import com.lesath.apps.util.HTTPMethod;
+
+public class TestCancelOrgMeeting {
+
+	@Test
+	public void deleteMeeting() throws Exception {
+		TestAPIGatewayRequest request = new TestAPIGatewayRequest();
+		Meeting meeting = new Meeting("6969696969", null, LocalDateTime.now(),
+				LocalDateTime.now(), null, "TestBoi");
+
+		MeetingDAO dao = new MeetingDAO();
+		String meetingId = dao.addMeeting(meeting);
+
+		request.addQueryParameter("scheduleId", "82e712aa-d528-4f19-a44c-0835f54b91ee");
+		request.addQueryParameter("meetingId", meetingId);
+
+		InputStream input = request.generateRequest(HTTPMethod.DELETE);
+		OutputStream output = new ByteArrayOutputStream();
+		Context context = request.generateContext("CreateScheduleDELETE");
+
+		CreateMeetingHandler handler = new CreateMeetingHandler();
+		handler.handleRequest(input, output, context);
+
+		LambdaResponse respose = LambdaHandler.gson.fromJson(output.toString(), LambdaResponse.class);
+
+		Assert.assertEquals(respose.statusCode, 204);
+	}
+}
