c4f51e790177e6b6f8f2c46912072d5353ee96b6
Ankur Gupta <rukna1000@gmail.com>
2018-12-11 22:18:10 -0500

    Create file for handler

---
+++ src/main/java/com/lesath/apps/controller/participantSearchSchedule/ParticipantSearchScheduleHandler.java
@@ -0,0 +1,41 @@
+package com.lesath.apps.controller.participantSearchSchedule;
+
+import com.lesath.apps.controller.APIGatewayRequest;
+import com.lesath.apps.controller.LambdaHandler;
+import com.lesath.apps.util.HTTPMethod;
+
+import java.util.ArrayList;
+
+public class ParticipantSearchScheduleHandler extends LambdaHandler {
+    @Override
+    protected boolean init() {
+        this.controllerName = "Participant Search Schedule";
+
+        this.handledMethods = new ArrayList<>();
+        this.handledMethods.add(HTTPMethod.POST);
+
+        return true;
+    }
+
+    @Override
+    protected boolean handlePUT(APIGatewayRequest request) {
+        return false;
+    }
+
+    @Override
+    protected boolean handlePOST(APIGatewayRequest request) {
+        
+
+        return false;
+    }
+
+    @Override
+    protected boolean handleGET(APIGatewayRequest request) {
+        return false;
+    }
+
+    @Override
+    protected boolean handleDELETE(APIGatewayRequest request) {
+        return false;
+    }
+}
