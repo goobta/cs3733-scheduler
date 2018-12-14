3f83af90540c20a84e389eeece5872629b218217
Ankur Gupta <rukna1000@gmail.com>
2018-12-13 22:21:34 -0500

    Fix off by 5 error

--- src/main/java/com/lesath/apps/controller/participantSearchSchedule/ParticipantSearchScheduleRequest.java
+++ src/main/java/com/lesath/apps/controller/participantSearchSchedule/ParticipantSearchScheduleRequest.java
@@ -72,7 +72,7 @@ public class ParticipantSearchScheduleRequest {
 
             System.out.println("Instant: " + instant + " Status: " + valid);
 
-            if(valid) availableSlots.add(instant.plusHours(5));
+            if(valid) availableSlots.add(instant);
         }
 
         return availableSlots;
